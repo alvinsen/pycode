d764c22fe7059d5500328d11c86e944b18c6ce3f
alvinsen <shen.yongquan@lezyo.com>
2015-06-04 11:25:01 +0800

    Contains Duplicate

---
+++ leetcode/217_ContainsDuplicate.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python
+# -*- coding:utf-8 -*-
+import time
+import timeit
+"""
+Contains Duplicate
+Given an array of integers, find if the array contains any duplicates. 
+Your function should return true if any value appears at least twice in the array, 
+and it should return false if every element is distinct.
+
+"""
+class Solution(object):
+    # @param {integer[]} nums
+    # @return {boolean}
+    def containsDuplicate(self, nums):
+        nums_set = set(list(nums))
+        if len(nums_set) < len(nums):
+            return True
+        return False
+
+    def containsDuplicate2(self, nums):
+        dic = {}
+        for num in nums:
+            res = dic.get(num, None)
+            if res is not None:
+                return True
+            dic[num] = 1
+        return False
+
+if __name__ == '__main__':
+    sol = Solution()
+    nums = [1,2,3,1]
+
+    start_time = time.time()
+    result = sol.containsDuplicate(nums)
+    use_time = time.time() - start_time
+
+    print 'nums is : %s, 是否有重复值： %s' %(nums, result)
+    print '耗时：%s' %use_time
+
+    start_time = time.time()
+    result = sol.containsDuplicate2(nums)
+    use_time = time.time() - start_time
+
+    print 'nums is : %s, 是否有重复值： %s' %(nums, result)
+    print '耗时：%s' %use_time
+
+    
